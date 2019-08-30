425a84aa94d5201f62d31f0a76b1eb9dee8c79b1
Y.D Zhang <424355505@qq.com>
2019-08-30 18:41:32 +0200

    Finished S3 S4 S5 but there are still some bugs to be fix.

--- D_ProjectFolder/project_reti_logiche.sim/sim_1/behav/xsim/xsim.dir/project_tb_behav/obj/xsim_1.c
+++ D_ProjectFolder/project_reti_logiche.sim/sim_1/behav/xsim/xsim.dir/project_tb_behav/obj/xsim_1.c
@@ -44,22 +44,22 @@
 #endif
 typedef void (*funcp)(char *, char *);
 extern int main(int, char**);
-extern void execute_24(char*, char *);
-extern void execute_25(char*, char *);
-extern void execute_26(char*, char *);
-extern void execute_21(char*, char *);
-extern void execute_22(char*, char *);
-extern void execute_23(char*, char *);
+extern void execute_41(char*, char *);
+extern void execute_42(char*, char *);
+extern void execute_43(char*, char *);
+extern void execute_38(char*, char *);
+extern void execute_39(char*, char *);
+extern void execute_40(char*, char *);
 extern void vhdl_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
 extern void transaction_4(char*, char*, unsigned, unsigned, unsigned);
-funcp funcTab[8] = {(funcp)execute_24, (funcp)execute_25, (funcp)execute_26, (funcp)execute_21, (funcp)execute_22, (funcp)execute_23, (funcp)vhdl_transfunc_eventcallback, (funcp)transaction_4};
+funcp funcTab[8] = {(funcp)execute_41, (funcp)execute_42, (funcp)execute_43, (funcp)execute_38, (funcp)execute_39, (funcp)execute_40, (funcp)vhdl_transfunc_eventcallback, (funcp)transaction_4};
 const int NumRelocateId= 8;
 
 void relocate(char *dp)
 {
 	iki_relocate(dp, "xsim.dir/project_tb_behav/xsim.reloc",  (void **)funcTab, 8);
-	iki_vhdl_file_variable_register(dp + 1053296);
-	iki_vhdl_file_variable_register(dp + 1053352);
+	iki_vhdl_file_variable_register(dp + 1054392);
+	iki_vhdl_file_variable_register(dp + 1054448);
 
 
 	/*Populate the transaction function pointer field in the whole net structure */
--- D_ProjectFolder/project_reti_logiche.srcs/sources_1/new/project_reti_logiche.vhd
+++ D_ProjectFolder/project_reti_logiche.srcs/sources_1/new/project_reti_logiche.vhd
@@ -47,7 +47,11 @@ signal next_state : state_type := IDLE;
 signal current_state : state_type := IDLE;
 signal operand_valid : std_logic := '0';
 signal todo_output : std_logic := '0';
-
+signal index_masc : integer range 0 to 7;
+signal distance_min : std_logic_vector(15 downto 0) := (others => '0');
+signal difference_value :std_logic_vector(15 downto 0) := (others => '0');
+signal punt_centroide_x : std_logic_vector(7 downto 0) := (others => '0');
+signal punt_centroide_y : std_logic_vector(7 downto 0) := (others => '0');
 
 begin
     --state reg
@@ -110,15 +114,16 @@ begin
 
     ---Define state
     delta:process(current_state,i_clk)
-    variable masc_di_uscita : std_logic_vector(7 downto 0) := (others => '0');
+    variable masc_di_entrata : std_logic_vector(7 downto 0) := (others => '0');
     variable punt_da_valutare_x : std_logic_vector(7 downto 0) := (others => '0');
     variable punt_da_valutare_y : std_logic_vector(7 downto 0) := (others => '0');
-    variable punt_centroide_x : std_logic_vector(7 downto 0) := (others => '0');
-    variable punt_centroide_y : std_logic_vector(7 downto 0) := (others => '0');
-    variable distance_min : std_logic_vector(15 downto 0) := (others => '0');
-    variable index_masc : integer range 0 to 7;
-   
     
+    
+    
+    
+    variable masc_di_uscita : std_logic_vector(7 downto 0) := (others => '0');
+    
+   
     begin
             case current_state is
                   when IDLE =>
@@ -130,20 +135,22 @@ begin
                      o_data <= (others => '0');       
                      operand_valid <= '0';
                      todo_output <= '0';
-                     masc_di_uscita := (others => '0');
+                     masc_di_entrata := (others => '0');
                      punt_da_valutare_x := (others => '0');
                      punt_da_valutare_y := (others => '0');
-                     punt_centroide_x := (others => '0');
-                     punt_centroide_y := (others => '0');
-                     distance_min := (others => '0');
-                     index_masc := 0;
+                     punt_centroide_x <= (others => '0');
+                     punt_centroide_y <= (others => '0');
+                     distance_min <= std_logic_vector(to_unsigned( 512 , 16));
+                     index_masc <= 0;
+                     difference_value <= (others => '0');
+                     masc_di_uscita := (others => '0');
                      if i_start = '1' then
                         o_en <= '1';
                      end if;
                       
                   when S0 =>
                      --Read maschera valore
-                     masc_di_uscita := i_data;
+                     masc_di_entrata := i_data;
                      o_address <= std_logic_vector(to_unsigned(17,16));
                      
                   when S1 =>
@@ -156,21 +163,63 @@ begin
                      punt_da_valutare_y := i_data;
                      
                   when S3 =>
-                      if masc_di_uscita(index_masc) = '0' then
-                         index_masc := index_masc + 1;
+                      punt_centroide_x <= (others => '0');
+                      punt_centroide_y <= (others => '0');
+                      if masc_di_entrata(index_masc) = '0' then
+                         operand_valid <= '0';
+                         index_masc <= index_masc + 1;
                       else
                          o_address <= std_logic_vector(to_unsigned(2*index_masc+1,16));
                          operand_valid <= '1';
                       end if;
                      
-                     if index_masc = 7 then
+                     if index_masc >= 7 then
                         todo_output <= '1';
                      end if;
                   
                   when S4 =>
+                     punt_centroide_x <= i_data;
+                     if punt_da_valutare_x > punt_centroide_x then
+                        difference_value <= punt_da_valutare_x - punt_centroide_x + "0000000000000000";
+                     else
+                        difference_value <= punt_centroide_x - punt_da_valutare_x + "0000000000000000";
+                     end if;
+                     
+                     if distance_min >= difference_value then
+                        operand_valid <= '1';
+                        o_address <= std_logic_vector(to_unsigned(2*index_masc+2,16));
+                     else
+                        operand_valid <= '0';
+                        if todo_output = '0'and rising_edge(i_clk) then
+                            index_masc <= index_masc + 1;
+                        end if;
+                     end if;
                   
                   when S5 =>
+                    punt_centroide_y <= i_data;
+                    if punt_da_valutare_y > punt_centroide_y then
+                        difference_value <= (punt_da_valutare_y - punt_centroide_y) + difference_value;
+                     else
+                        difference_value <= (punt_centroide_y - punt_da_valutare_y) + difference_value;
+                     end if;
+                     
+                     if distance_min > difference_value then
+                        distance_min <= difference_value;
+                        masc_di_uscita := (others => '0');
+                        masc_di_uscita(index_masc) := '1';
+                     elsif distance_min = difference_value then
+                        masc_di_uscita(index_masc) := '1';
+                     else
+                        --do nothing
+                     end if;
+                     
+                     operand_valid <= '0';
+                     if todo_output = '0' and rising_edge(i_clk) then
+                            index_masc <= index_masc + 1;
+                     end if;
+                  
                   when S6 =>
+                        -- Todo output
             end case;
           
     end process delta;
